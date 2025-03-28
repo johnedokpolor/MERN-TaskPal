5178287387db6e9c32daba63de4e431e7754e652
John <johnedokpolor@gmail.com>
2025-03-28 02:23:52 +0100

    server url change

--- frontend/src/store/authStore.ts
+++ frontend/src/store/authStore.ts
@@ -1,8 +1,8 @@
 import { create } from "zustand";
 import axios, { AxiosError } from "axios";
 
-export const API_URL = "http://localhost:1000/api/auth";
-// export const API_URL = "https://mern-taskpal-backend.onrender.com/api/auth";
+// export const API_URL = "http://localhost:1000/api/auth";
+export const API_URL = "https://mern-taskpal-backend.onrender.com/api/auth";
 export interface User {
   _id: string;
   name: string;
